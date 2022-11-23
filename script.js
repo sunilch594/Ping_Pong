211029d86c9307ddde4ae1d73c82e9e0ed4dff91
Sunil kumar <86351928+sunilch594@users.noreply.github.com>
2022-11-23 10:17:50 +0530

    Add files via upload
    This App is most suitable & responsive for the
    Desktop Version.

---
+++ script.js
@@ -0,0 +1,143 @@
+var ball = document.getElementById('ball');
+var rod1 = document.getElementById('rod1');
+var rod2 = document.getElementById('rod2');
+const start = document.querySelector('.start');
+
+const storeName = window.prompt("Enter Your Name:")
+const storeScore = "PPMaxScore";
+const rod1Name = "Rod 1";
+const rod2Name = "Rod 2";
+var snd = new Audio("sound/computer-love-121106.mp3");
+
+let score,
+    maxScore,
+    movement,
+    rod,
+    ballSpeedX = 2,
+    ballSpeedY = 2;
+
+let gameOn = false;
+
+let windowWidth = window.innerWidth,
+    windowHeight = window.innerHeight;
+
+(function () {
+    rod = localStorage.getItem(storeName);
+    maxScore = localStorage.getItem(storeScore);
+
+    if (rod === "null" || maxScore === "null") {
+        alert("This is the first time you are playing this game. LET'S START");
+        maxScore = 0;
+        rod = "Rod1";
+    }
+    resetBoard(rod);
+})();
+
+function resetBoard(rodName) {
+
+    rod1.style.left = (window.innerWidth - rod1.offsetWidth) / 2 + 'px';
+    rod2.style.left = (window.innerWidth - rod2.offsetWidth) / 2 + 'px';
+    ball.style.left = (windowWidth - ball.offsetWidth) / 2 + 'px';
+
+    if (rodName === rod2Name) {
+        ball.style.top = (rod1.offsetTop + rod1.offsetHeight) + 'px';
+        ballSpeedY = 2;
+    } else if (rodName === rod1Name) {
+        ball.style.top = (rod2.offsetTop - rod2.offsetHeight) + 'px';
+        ballSpeedY = -2;
+    }
+    start.style.display = "block"
+    score = 0;
+    gameOn = false;
+
+}
+
+function storeWin(rod, score) {
+
+    if (score > maxScore) {
+        maxScore = score;
+        localStorage.setItem(storeName, rod);
+        localStorage.setItem(storeScore, maxScore);
+    }
+
+    clearInterval(movement);
+	snd.pause();
+
+    resetBoard(rod);
+
+    alert("---------------------GAME OVER"+"  "+storeName.toUpperCase()+"--------------------"+ "\nYour Score is "+ (score * 100) + ".    Max score is: " + (maxScore * 100));
+
+}
+
+window.addEventListener('keypress', function (event) {
+    let rodSpeed = 20;
+    console.log(event.code)
+
+    let rodRect = rod1.getBoundingClientRect();
+
+    if (event.code === "KeyD" && ((rodRect.x + rodRect.width) < window.innerWidth)) {
+        rod1.style.left = (rodRect.x) + rodSpeed + 'px';
+        rod2.style.left = rod1.style.left;
+    } else if (event.code === "KeyA" && (rodRect.x > 0)) {
+        rod1.style.left = (rodRect.x) - rodSpeed + 'px';
+        rod2.style.left = rod1.style.left;
+    }
+
+    if (event.code === "Enter") {
+        if (!gameOn) {
+            gameOn = true;
+            start.style.display = 'none';
+            let ballRect = ball.getBoundingClientRect();
+            let ballX = ballRect.x;
+            let ballY = ballRect.y;
+            let ballDia = ballRect.width;
+
+            let rod1Height = rod1.offsetHeight;
+            let rod2Height = rod2.offsetHeight;
+            let rod1Width = rod1.offsetWidth;
+            let rod2Width = rod2.offsetWidth;
+         	snd.play();
+
+            movement = setInterval(function () {
+                // Move ball 
+                ballX += ballSpeedX;
+                ballY += ballSpeedY;
+
+                let rod1X = rod1.getBoundingClientRect().x;
+                let rod2X = rod2.getBoundingClientRect().x;
+
+                ball.style.left = ballX + 'px';
+                ball.style.top = ballY + 'px';
+
+                if ((ballX + ballDia) > windowWidth || ballX < 0) {
+                    ballSpeedX = -ballSpeedX; // Reverses the direction
+                }
+
+                // It specifies the center of the ball on the viewport
+                let ballPos = ballX + ballDia / 2;
+
+                // Check for Rod 1
+                if (ballY <= rod1Height) {
+                    ballSpeedY = -ballSpeedY; // Reverses the direction
+                    score++;
+
+                    // Check if the game ends
+                    if ((ballPos < rod1X) || (ballPos > (rod1X + rod1Width))) {
+                        storeWin(rod2Name, score);
+                    }
+                }
+
+                // Check for Rod 2
+                else if ((ballY + ballDia) >= (windowHeight - rod2Height)) {
+                    ballSpeedY = -ballSpeedY; // Reverses the direction
+                    score++;
+
+                    // Check if the game ends
+                    if ((ballPos < rod2X) || (ballPos > (rod2X + rod2Width))) {
+                        storeWin(rod1Name, score);
+                    }
+                }
+            }, 10);
+        }
+    }
+});
